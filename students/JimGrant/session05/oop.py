df6be6774bb70710e6ae13e34e83284ad26fcedb
Jim Grant <jimgrant@gmail.com>
2015-04-02 16:17:27 -0700

    complete oop.py

--- students/JimGrant/session05/oop.py
+++ students/JimGrant/session05/oop.py
@@ -1,9 +1,31 @@
 def question01():
-    pass
+    """Can I have classes nested inside classes as an attribute?"""
+    class Question1(object):
+        class NestClass(object):
+            x = 1
+
+        def __init__(self):
+            self.nestclass = self.NestClass()
+
+    print("Can I have classes nested inside classes as an attribute?")
+    question1 = Question1()
+    print(question1.nestclass.x)
+    print("Answer: Yes")
 
 
 def question02():
-    pass
+    """What happens if I instantiate a class inside its own __init__()?"""
+    class Question2(object):
+        def __init__(self):
+            self.x = Question2()
+
+    print("What happens if I instantiate a class inside its own __init__()?")
+    try:
+        question2 = Question2()
+    except RuntimeError:
+        print("Answer: You will very quickly exceed recursion depth limits.")
+    else:
+        print("Nothing! Stop worrying so much!")
 
 
 if __name__ == "__main__":
