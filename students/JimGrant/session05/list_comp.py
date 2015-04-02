fa17d779823ab03d05e933b19882a180edd45f91
Jim Grant <jimgrant@gmail.com>
2015-04-02 10:03:14 -0700

    add framework and tests

---
+++ students/JimGrant/session05/list_comp.py
@@ -0,0 +1,9 @@
+def count_evens(nums):
+    # one line comprehension here
+    pass
+
+if __name__ == "__main__":
+    assert count_evens([2, 1, 2, 3, 4]) == 3
+    assert count_evens([2, 2, 0]) == 3
+    assert count_evens([1, 3, 5]) == 0
+    print("All tests pass")
