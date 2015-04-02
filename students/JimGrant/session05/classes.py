d8972c242544360c73a6577d879396bc82b190a5
Jim Grant <jimgrant@gmail.com>
2015-04-02 16:17:45 -0700

    complete classes.py

--- students/JimGrant/session05/classes.py
+++ students/JimGrant/session05/classes.py
@@ -1,5 +1,20 @@
 def question01():
-    pass
+    """Can I have class attributes and instance attributes
+    named the same thing?
+    """
+    class Question1(object):
+        x = 1
+
+        def __init__(self):
+            self.x = 2
+
+    print("Can I have class attributes and instance attributes "
+          "named the same thing?")
+    question1 = Question1()
+    print(Question1.x)
+    print(question1.x)
+    print("Answer: Yes. Note that if the instance attribute didn't exist, "
+          "that would've printed 1 both times!")
 
 
 if __name__ == "__main__":
