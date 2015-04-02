4061a8bd21f07628a61f71f417195bb97cc2814e
Jim Grant <jimgrant@gmail.com>
2015-04-02 14:49:34 -0700

    add framework and tests

---
+++ students/JimGrant/session05/lambda.py
@@ -0,0 +1,11 @@
+def function_builder(n):
+    return []
+
+
+if __name__ == "__main__":
+    the_list = function_builder(4)
+    for f in the_list:
+        print(f(5))
+    assert(the_list[0](2) == 2)
+    assert(the_list[1](2) == 3)
+    print("All tests pass")
