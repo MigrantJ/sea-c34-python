c100c9af4ca1d93f5e1fe62c9cd37d484df571e1
Jim Grant <jimgrant@gmail.com>
2015-04-03 14:19:38 -0700

    progress on passing element to append. Html, Body, and P classes

--- students/JimGrant/session06/html_render.py
+++ students/JimGrant/session06/html_render.py
@@ -2,20 +2,42 @@
 
 
 class Element(object):
-    name = u""
+    tag = u""
     indent = u"    "
 
     def __init__(self, content=None):
-        self.content = self.indent + str(content) if content else content
+        self.content = self.indent + str(content) if content else ""
 
     def append(self, s):
+        add = ""
         try:
-            self.content = u"\n".join([self.content, self.indent + s])
-        except TypeError:
-            self.content = self.indent + s
+            add = self.indent + s.output(Element.indent)
+        except AttributeError:
+            add = self.indent + s
+        finally:
+            self.content = u"\n".join([self.content, add])
+
+    def output(self, ind=u""):
+        open_tag = u"".join([u"\n", ind, u"<", self.tag, u">"])
+        close_tag = u"".join([ind, u"</", self.tag, u">"])
+        output = u"\n".join([open_tag, self.content, close_tag])
+        return output
 
     def render(self, file_out, ind=u""):
-        open_tag = u"".join([u"\n", ind, u"<", self.name, u">"])
-        close_tag = u"".join([ind, u"</", self.name, u">"])
-        output = u"\n".join([open_tag, self.content, close_tag])
-        file_out.write(output)
+        file_out.write(self.output(ind))
+
+
+class Html(Element):
+    header = u"<!DOCTYPE html>"
+    tag = u"html"
+
+    def output(self, ind=u""):
+        return Html.header + Element.output(self, ind)
+
+
+class Body(Element):
+    tag = u"body"
+
+
+class P(Element):
+    tag = u"p"
