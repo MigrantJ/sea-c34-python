09834235e9942c56a5a4d76d771432004f64ac73
Jim Grant <jimgrant@gmail.com>
2015-04-05 10:59:13 -0700

    progress on step 2, going to try content as list refactor

--- students/JimGrant/session06/html_render.py
+++ students/JimGrant/session06/html_render.py
@@ -6,25 +6,30 @@ class Element(object):
     indent = u"    "
 
     def __init__(self, content=None):
-        self.content = self.indent + str(content) if content else ""
+        self.indent = ""
+        self.content = Element.indent + str(content) if content else ""
 
     def append(self, s):
         add = ""
         try:
-            add = self.indent + s.output(Element.indent)
+            s.indent = Element.indent + self.indent
+            add = s.output(s.indent)
         except AttributeError:
-            add = self.indent + s
+            add = Element.indent + s
         finally:
-            self.content = u"\n".join([self.content, add])
+            if self.content != "":
+                self.content = u"\n".join([self.content, add])
+            else:
+                self.content = add
 
     def output(self, ind=u""):
-        open_tag = u"".join([u"\n", ind, u"<", self.tag, u">"])
+        open_tag = u"".join([ind, u"<", self.tag, u">"])
         close_tag = u"".join([ind, u"</", self.tag, u">"])
         output = u"\n".join([open_tag, self.content, close_tag])
         return output
 
     def render(self, file_out, ind=u""):
-        file_out.write(self.output(ind))
+        file_out.write(self.output(self.indent + ind))
 
 
 class Html(Element):
@@ -32,7 +37,7 @@ class Html(Element):
     tag = u"html"
 
     def output(self, ind=u""):
-        return Html.header + Element.output(self, ind)
+        return u"\n".join([Html.header, Element.output(self, ind)])
 
 
 class Body(Element):
