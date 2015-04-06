8010cbd34b08841e76269a96f3919ebb7eb641f3
Jim Grant <jimgrant@gmail.com>
2015-04-06 10:08:35 -0700

    complete properties.py

--- students/JimGrant/session06/properties.py
+++ students/JimGrant/session06/properties.py
@@ -1,6 +1,15 @@
+class Example(object):
+    def __init__(self, x):
+        self._x = x
+
+    x = property(lambda self: self._x, doc="read only")
+
+
 def question01():
-    """docstring"""
-    pass
+    """Can I use lambdas for getters / setters in property()?"""
+    print("Can I use lambdas for getters / setters in property()?")
+    example = Example("Answer: Yes you can")
+    print(example.x)
 
 
 if __name__ == "__main__":
