2c4081472dbc9f2c05cc86ee93ef69ee538b3681
Jim Grant <jimgrant@gmail.com>
2015-04-06 10:40:04 -0700

    complete static.py

--- students/JimGrant/session06/static.py
+++ students/JimGrant/session06/static.py
@@ -1,6 +1,17 @@
-def question01():
-    """docstring"""
+class Example(object):
+    @staticmethod
+    def yesreturner():
+        return "Yes"
+
+
+class SubExample(Example):
     pass
+
+
+def question01():
+    """Do sub-classes inherit static methods?"""
+    print("Do sub-classes inherit static methods?")
+    print("Answer: {}".format(SubExample.yesreturner()))
 
 
 if __name__ == "__main__":
