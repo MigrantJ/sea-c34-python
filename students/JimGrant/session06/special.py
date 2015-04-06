6e16b1fef2a772102cf3d60dca2244b6dfe41566
Jim Grant <jimgrant@gmail.com>
2015-04-06 10:22:26 -0700

    complete special.py

--- students/JimGrant/session06/special.py
+++ students/JimGrant/session06/special.py
@@ -1,6 +1,22 @@
+class Addable(object):
+    def __init__(self, value):
+        self.value = value
+
+    def __add__(self, other):
+        return Addable(self.value + other)
+
+    def __str__(self):
+        return str(self.value)
+
 def question01():
-    """docstring"""
-    pass
+    """If I implement __add__ in a class, can I use the + operator
+    to add it to integers?
+    """
+    print("If I implement __add__ in a class, can I use the + operator "
+          "to add it to integers?")
+    inst = Addable(5)
+    print("This should be seven: {}".format(inst + 2))
+    print("Answer: Yes. Wow this is cool!")
 
 
 if __name__ == "__main__":
