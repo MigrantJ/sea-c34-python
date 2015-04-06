db5bfaf1ddced070959782d051cd98e47e084267
Jim Grant <jimgrant@gmail.com>
2015-04-06 11:35:27 -0700

    complete multiple.py

--- students/JimGrant/session06/multiple.py
+++ students/JimGrant/session06/multiple.py
@@ -1,6 +1,32 @@
+class Animal(object):
+    def __init__(self, name):
+        self.name = name
+
+    def announce(self):
+        print("I am a {}".format(self.name))
+
+
+class FlyMixin(object):
+    def fly(self):
+        print("I'm flying")
+
+
+class SwimMixin(object):
+    def swim(self):
+        print("I'm swimming")
+
+
+class FlyingFish(SwimMixin, FlyMixin, Animal):
+    def __init__(self):
+        Animal.__init__(self, "flying fish")
+
+
 def question01():
-    """docstring"""
-    pass
+    """What would an example of mixins look like?"""
+    flyingfish = FlyingFish()
+    flyingfish.announce()
+    flyingfish.swim()
+    flyingfish.fly()
 
 
 if __name__ == "__main__":
