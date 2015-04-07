ecab010a3f3a226d8746836c0ff83ec28065deaa
Jim Grant <jimgrant@gmail.com>
2015-04-07 10:23:33 -0700

    complete circle class

--- students/JimGrant/session07/circle.py
+++ students/JimGrant/session07/circle.py
@@ -7,4 +7,35 @@ import math
 
 
 class Circle(object):
-    pass
+    def __init__(self, radius):
+        self.radius = float(radius)
+
+    def __str__(self):
+        return u"Circle with radius: %.6f" % self.radius
+
+    def __repr__(self):
+        return u"Circle(%i)" % self.radius
+
+    def __add__(self, other):
+        return Circle(self.radius + other.radius)
+
+    def __mul__(self, other):
+        return Circle(self.radius * other)
+
+    def __cmp__(self, other):
+        return int(self.radius - other.radius)
+
+    def __ne__(self, other):
+        return self.radius != other.radius
+
+    @property
+    def diameter(self):
+        return self.radius * 2
+
+    @diameter.setter
+    def diameter(self, value):
+        self.radius = value / 2
+
+    @property
+    def area(self):
+        return math.pi * (self.radius ** 2)
