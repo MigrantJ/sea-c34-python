3a9e22180c280a4d8a5655261e6a8bdc8c1eb31d
Jim Grant <jimgrant@gmail.com>
2015-04-07 11:10:07 -0700

    convert string addition to join

--- students/JimGrant/session07/rot13.py
+++ students/JimGrant/session07/rot13.py
@@ -5,8 +5,8 @@ l_alpha = "".join([chr(i) for i in range(97, 123)])
 # create the translation string, all chars offset by 13
 l_alpha_offset = l_alpha[13:] + l_alpha[:13]
 # include uppercase
-l_alpha += l_alpha.upper()
-l_alpha_offset += l_alpha_offset.upper()
+l_alpha = "".join([l_alpha, l_alpha.upper()])
+l_alpha_offset = "".join([l_alpha_offset, l_alpha_offset.upper()])
 
 
 def rot13(input_string):
